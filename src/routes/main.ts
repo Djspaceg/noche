5afffee41252a25251357e57353a6ae77004c331
Blake Stephens <stepblk@amazon.com>
2023-02-14 11:28:59 -0800

    Improved 404 handling by matching the nearest directory target and serving that instead

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,14 +1,29 @@
 import e from 'express';
 import { existsSync, statSync } from 'fs';
-import { OutgoingHttpHeader, OutgoingHttpHeaders } from 'http';
-import { join, normalize } from 'path';
 import { parse } from 'url';
 import conf from '../configuration';
 import { getDirectory, hasIndex } from '../extensions/directory-indexing';
+import path from 'path';
+
+const getClosestMatchingDirectory = function (filePath: string): string {
+  const pathParts = path.parse(filePath);
+
+  if (!existsSync(pathParts.dir)) {
+    return getClosestMatchingDirectory(pathParts.dir);
+  }
+  return pathParts.dir;
+};
 
 const isServable = function (filePath: string) {
   const fileExists = existsSync(filePath);
   if (!fileExists) {
+    const nearestPath = getClosestMatchingDirectory(filePath);
+    if (nearestPath) {
+      console.log(
+        `File "${filePath}" not found. Nearest resolvable path to be used is "${nearestPath}".`
+      );
+      return nearestPath;
+    }
     return false;
   }
   const rawStats = statSync(filePath);
@@ -20,8 +35,8 @@ const isServable = function (filePath: string) {
 
 export function main(req: e.Request, res: e.Response) {
   const objUrl = parse(req.url, true);
-  const uri = decodeURI(normalize(objUrl.pathname || ''));
-  const filename = join(conf.DocumentRoot, uri);
+  const uri = decodeURI(path.normalize(objUrl.pathname || ''));
+  const filename = path.join(conf.DocumentRoot, uri);
 
   // console.log("Running index.js because of",req.url);
 
@@ -39,14 +54,18 @@ export function main(req: e.Request, res: e.Response) {
     res.end();
   };
 
-  const bitIsServable = isServable(filename);
-  if (bitIsServable) {
-    if (bitIsServable === 'maybe') {
-      const bitHasIndex = hasIndex(filename);
+  const fileIsServable = isServable(filename);
+  if (fileIsServable) {
+    if (typeof fileIsServable === 'string') {
+      let probablyServableFolder = filename;
+      if (fileIsServable !== 'maybe') {
+        probablyServableFolder = fileIsServable;
+      }
+      const bitHasIndex = hasIndex(probablyServableFolder);
       if (bitHasIndex) {
-        res.sendFile(filename);
+        res.sendFile(probablyServableFolder);
       } else {
-        getDirectory(filename, (files) => {
+        getDirectory(probablyServableFolder, (files) => {
           if (typeof files === 'string') {
             // writeEntireResponse(files);
             res.render('index', {
